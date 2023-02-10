5eaa78fab751e4d9b3d030d56f115e9c484e3f64
kamula <kamulaisaac@gmail.com>
2023-02-10 11:37:08 +0300

    remove accounts app & create authentication app

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts.apps.AccountsConfig',
     'transfers.apps.TransfersConfig',
 ]
 
