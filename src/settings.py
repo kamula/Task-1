e34bfb5b9dc642ac848ea9cdf0c108312e451d6f
kamula <kamulaisaac@gmail.com>
2023-02-10 12:09:05 +0300

    auth user model configuration in root project settings

--- src/settings.py
+++ src/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'transfers.apps.TransfersConfig',
 ]
 
+AUTH_USER_MODEL = 'authentication.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
