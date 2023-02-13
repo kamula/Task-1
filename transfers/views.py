c1988ae108f7b6ab3c6826307a8d12c821a0c7c0
kamula <kamulaisaac@gmail.com>
2023-02-13 11:41:26 +0300

    create account view

--- transfers/views.py
+++ transfers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .serializers import AccountCreationSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+@swagger_auto_schema(methods=['post'], request_body=AccountCreationSerializer)
+@api_view(['POST'])  # define the HTTP method of accessing the view
+def create_account_view(request):
+    '''Function for Creating Account'''
+    resp = {}
+    return Response(resp, status=status.HTTP_400_BAD_REQUEST)
