e24e54afe58c52336475c73405725fd161651c59
kamula <kamulaisaac@gmail.com>
2023-02-13 14:20:35 +0300

    deserialize user data

--- transfers/views.py
+++ transfers/views.py
@@ -1,4 +1,4 @@
-from .serializers import AccountCreationSerializer
+from .serializers import AccountCreationSerializer,GetAccountCreationSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from drf_yasg import openapi
+from authentication.models import User
 from . models import Account
 from . utils import get_account_from_account_id
 from rest_framework.decorators import api_view, permission_classes
@@ -57,9 +58,11 @@ def get_account_details(request,id):
     # account = get_account_from_account_id(id)
     account = Account.objects.get(id=id)
     if account:
-        account_serializer = AccountCreationSerializer(account)
+        account_serializer = GetAccountCreationSerializer(account)
+        account_data = account_serializer.data
+        account_data['user'] = User.objects.get(id= account.user.id).full_name
         resp['status'] = 'success'        
-        resp['account'] = account_serializer.data
+        resp['account'] = account_data
         return Response(resp, status=status.HTTP_200_OK)
     else:
         resp['status'] = 'fail'
