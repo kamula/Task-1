ccdb87b84b0f9c0998b7fc316246cc0398c1ff92
kamula <kamulaisaac@gmail.com>
2023-02-13 14:35:47 +0300

    create transfer serializer

--- transfers/serializers.py
+++ transfers/serializers.py
@@ -2,22 +2,27 @@
 
 from rest_framework import serializers
 from authentication.models import User
-from . models import Account
-
+from . models import Account, Transfer
 
 
 class AccountCreationSerializer(serializers.ModelSerializer):
     '''Class for Serializing & Deserializing  Account object'''
     class Meta:
         model = Account
-        fields = ['id','user', 'starting_balance', 'date_created']
+        fields = ['id', 'user', 'starting_balance', 'date_created']
         read_only_fields = ['date_created']
 
 
 class GetAccountCreationSerializer(serializers.ModelSerializer):
     '''Class for  Deserializing  Account object'''
-    
+
     class Meta:
         model = Account
-        fields = ['id','user', 'starting_balance', 'date_created']
+        fields = ['id', 'user', 'starting_balance', 'date_created']
         read_only_fields = ['date_created']
+
+
+class CreateTransferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transfer
+        fields = ['sender_account', 'receiving_account', 'amount']
