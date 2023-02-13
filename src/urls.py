9031b5a52eff7af090a255e7f283a3427940cbe2
kamula <kamulaisaac@gmail.com>
2023-02-13 09:54:01 +0300

    API schema url & user urls base mapping

--- src/urls.py
+++ src/urls.py
@@ -14,8 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task 1 API",
+        default_version='v1',
+        description="API that implements a simple money transfer between accounts feature",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API schema URL
+    path('schema', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # define the authentication urls at the root URL mapper of the project
+    path('api/v1/users/', include('authentication.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
